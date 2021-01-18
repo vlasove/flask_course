d8c5fb04f9db2e4961125b0bf08021430aaf3cf3
Evgeny Vlasov <evgeny_vlasov@yahoo.com>
2021-01-18 20:38:59 +0300

    init

---
+++ PreLec0/main.py
@@ -0,0 +1,9 @@
+"""
+Решение задачи A
+"""
+name = input("Please enter name: ")
+lastname = input("Please enter lastname: ")
+age = int(input())
+
+message = f"Имя: {name} , Фамилия: {lastname} , Возраст: {age} . Студент BPS"
+print(message)
