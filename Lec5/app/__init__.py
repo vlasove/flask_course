ef489fc099641f3535fd5e762ce8b0b2c69f15a8
Evgeny Vlasov <evgeny_vlasov@yahoo.com>
2021-01-21 19:20:54 +0300

    added Lec4 + templates

--- Lec5/app/__init__.py
+++ Lec5/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask 
+from conf import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes 
 
--- Lec5/app/routes.py
+++ Lec5/app/routes.py
@@ -1,5 +1,6 @@
 from app import app 
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+from app.forms import PostForm, LoginForm
 
 # Допустим это таблица в бд
 posts = [
@@ -22,6 +23,40 @@ user = {
     'email' : 'bob@mail.ru',
     'age' : 33,
 }
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        new_username = form.username.data 
+        user['username'] = new_username 
+        flash(f'Successfully logged-in as {new_username}')
+        return redirect(url_for('posts_list'))
+        
+    return render_template(
+        'login.html',
+        form=form,
+    )
+
+@app.route('/posts/create', methods=['GET', 'POST'])
+def post_create():
+    form = PostForm()
+    if request.method == 'POST' and form.validate():
+        new_post_author = form.author.data
+        new_post_body = form.body.data
+
+        new_post = {
+            'author' : new_post_author,
+            'body' : new_post_body,
+        } 
+        posts.append(new_post)
+        flash('Post successfully created!')
+        return redirect(url_for('posts_list'))
+
+    return render_template(
+        'post_create.html',
+        form=form,
+    )
 
 @app.route('/posts')
 def posts_list():
